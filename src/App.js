a7901eaa0b1576fdaa5c6bafea1bbce555700bc4
akutuh <akutuh@uef.fi>
2022-08-02 04:11:21 +0300

    app template

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react' // tarvitsemme importin nyt myös kompontentin määrittelyn yhteydessä
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello webpack</h1>
+    </div>
+  )
+}
+
+export default App
